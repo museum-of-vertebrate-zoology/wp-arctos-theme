478d8819aa920384cb082c858f6d7677202aa9dd
Philip Kahn <tigerhawkvok@gmail.com>
2014-08-13 16:36:43 -0700

    Move dependencies from Subtree to Bower

--- update_dependencies.bat
+++ update_dependencies.bat
@@ -1,8 +1,3 @@
-git fetch purl master
-git subtree pull --prefix arctosdb.org/js/purl purl master --squash
-java -jar yuicompressor.jar arctosdb.org/js/purl/purl.js arctosdb.org/js/purl.min.js
-git fetch picturefill master
-git subtree pull --prefix arctosdb.org/js/picturefill picturefill master --squash
-copy /y arctosdb.org/js/picturefill/dist/picturefill.min.js arctosdb.org/js/picturefill.min.js
 cd arctosdb.org
 bower update
+java -jar yuicompressor.jar bower_components/purl/purl.js js/purl.min.js
