1013d054e42b504a50a76101a06d40dfb6a3f010
Philip Kahn <tigerhawkvok@gmail.com>
2014-10-17 15:41:57 -0700

    Add unix-compatible wrapper scripts

---
+++ update_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo Updating dependencies ... please wait.
+cd arctosdb.org
+bower update
+echo Minifying ...
+java -jar ../yuicompressor.jar bower_components/purl/purl.js -o js/purl.min.js
+java -jar ../yuicompressor.jar bower_components/sticky/jquery.sticky.js -o js/sticky.min.js
+cd .. 
+./run-compile.sh
