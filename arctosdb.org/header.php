99a5154ee6335a783c2235474f43ac93377b2b5d
Philip Kahn <tigerhawkvok@gmail.com>
2015-01-04 01:08:43 -0800

    Work on submenu pages and child pages

--- arctosdb.org/header.php
+++ arctosdb.org/header.php
@@ -117,4 +117,9 @@ echo "Theme version " . $my_theme->get( 'Version' );
                      wp_list_pages(array("child_of"=>get_the_id()));
                      echo "</ul>";
                    }
+$my_wp_query = new WP_Query();
+$all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));
+echo print_r(get_page_children(get_the_id(),$all_wp_pages),true);
+# http://pastebin.com/Jk7n20mB
+# https://wordpress.org/support/topic/wp_nav_menu-list-only-2nd-level-separate-submenu/page/2#post-1973991
            ?>
