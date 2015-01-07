e161a4c68aaa7c231a75523fc986a0a491c6032f
Philip Kahn <tigerhawkvok@gmail.com>
2015-01-06 22:13:50 -0800

    Close #2

--- arctosdb.org/wp-submenu-display.php
+++ arctosdb.org/wp-submenu-display.php
@@ -7,9 +7,13 @@
 if(!is_home())
   {
     require_once("Arctos_Submenu_Walker.php");
-
-    wp_nav_menu( array( "container"=>"nav","container_id"=>"submenu-navigation", 'container_class' => 'menu-submenu col-md-9 col-s-12', 'theme_location' => 'primary-header', "walker"=> new sub_nav_walker ) );
-
+    $parent = array_reverse(get_post_ancestors($post->ID));
+    # While this doesn't work right with random test heierarchies,
+    # this should be right on release.
+    $first_parent = get_page($parent[0]);
+    echo "<nav id='submenu-navigation' class='menu-submenu'><h4 id='submenu-nav-header'>".$first_parent->post_name." Subtopics:</h4>";
+    wp_nav_menu( array('theme_location' => 'primary-header', "walker"=> new sub_nav_walker ) );
+    echo "</nav>";
   }
 /* $my_wp_query = new WP_Query(); */
 /* $all_wp_pages = $my_wp_query->query(array('post_type' => 'page')); */
