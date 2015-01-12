58393e08d692fa720a31fcf2603a72b5485a6b99
Philip Kahn <tigerhawkvok@gmail.com>
2015-01-12 14:07:51 -0800

    Fix #5

--- arctosdb.org/sidebar.php
+++ arctosdb.org/sidebar.php
@@ -12,46 +12,57 @@
     <a id='twitter_embed' class="twitter-timeline" href="https://twitter.com/arctosdb" data-widget-id="344887340390551552" data-screen-name="arctosdb">Tweets by @arctosdb</a>
     <script type="text/javascript">!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
   </div>
-		<div id="primary" class="widget-area" role="complementary">
-			<ul class="xoxo">
+                <div id="primary" class="widget-area" role="complementary">
+                        <ul class="xoxo">
 
 <?php
-	/* When we call the dynamic_sidebar() function, it'll spit out
-	 * the widgets for that widget area. If it instead returns false,
-	 * then the sidebar simply doesn't exist, so we'll hard-code in
-	 * some default sidebar stuff just in case.
-	 */
-	if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
+        /* When we call the dynamic_sidebar() function, it'll spit out
+         * the widgets for that widget area. If it instead returns false,
+         * then the sidebar simply doesn't exist, so we'll hard-code in
+         * some default sidebar stuff just in case.
+         */
+        if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
+                        <li id="recent-posts" class="widget-container">                     
+                          <h3 class="widget-title">Recent Posts</h3>
+                          <ul>
+                            <?php
+                               $args = array("numberposts"=>3);
+                               $recent_posts = wp_get_recent_posts($args);
+                               foreach( $recent_posts as $recent ){
+                               echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
+                               }
+                               ?>
+                          </ul>
+                        </li>
+                        <li id="archives" class="widget-container">
+                                <h3 class="widget-title"><?php _e( 'Archives', 'twentyten' ); ?></h3>
+                                <ul>
+                                        <?php wp_get_archives( 'type=monthly' ); ?>
+                                </ul>
+                        </li>
 
-			<li id="archives" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Archives', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_get_archives( 'type=monthly' ); ?>
-				</ul>
-			</li>
+                        <li id="meta" class="widget-container">
+                                <h3 class="widget-title"><?php _e( 'Meta', 'twentyten' ); ?></h3>
+                                <ul>
+                                        <?php wp_register(); ?>
+                                        <li><?php wp_loginout(); ?></li>
+                                        <?php wp_meta(); ?>
+                                </ul>
+                        </li>
 
-			<li id="meta" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Meta', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</li>
-
-		<?php endif; // end primary widget area ?>
-			</ul>
-		</div><!-- #primary .widget-area -->
+                <?php endif; // end primary widget area ?>
+                        </ul>
+                </div><!-- #primary .widget-area -->
 
 <?php
-	// A second sidebar for widgets, just because.
-	if ( is_active_sidebar( 'secondary-widget-area' ) ) : ?>
+        // A second sidebar for widgets, just because.
+        if ( is_active_sidebar( 'secondary-widget-area' ) ) : ?>
 
-		<div id="secondary" class="widget-area" role="complementary">
-			<ul class="xoxo">
-				<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
-			</ul>
-		</div><!-- #secondary .widget-area -->
+                <div id="secondary" class="widget-area" role="complementary">
+                        <ul class="xoxo">
+                                <?php dynamic_sidebar( 'secondary-widget-area' ); ?>
+                        </ul>
+                </div><!-- #secondary .widget-area -->
 
 <?php endif; ?>
 </div>
