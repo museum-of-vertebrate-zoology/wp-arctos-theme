76cf0798b33fa0b6d382c8fe1eff035b837a92b9
Philip Kahn <tigerhawkvok@gmail.com>
2015-07-22 15:36:13 -0700

    Change the phrasing of the search form

--- arctosdb.org/searchform.php
+++ arctosdb.org/searchform.php
@@ -1,10 +1,10 @@
 <form role="search" method="get" id="sidebar-search-form" class="search-form" action="<?php echo home_url( '/' ); ?>" onsubmit="event.preventDefault()">
   <label for="s" class="screen-reader-text">Search for:</label>
-  <paper-input id='s' name='s' label="Search this"></paper-input>
+  <paper-input id='s' name='s' label="Search this website"></paper-input>
   <paper-fab class='mini foundersrock' id='searchsubmit' icon='icons:search' raisedButton></paper-fab>
 </form>
 <form role="search" method="get" id="arctos-search-form" class="search-form" action="http://arctos.database.museum/SpecimenResults.cfm" onsubmit="event.preventDefault()">
   <label for="arctos-search" class="screen-reader-text">Arctos Quick Search:</label>
-  <paper-input id='arctos-search' name='arctos-search' label="Search Arctos database"></paper-input>
+  <paper-input id='arctos-search' name='arctos-search' label="Search the database"></paper-input>
   <paper-fab class='mini foundersrock' id='searchsubmit-arctos' icon='icons:search' raisedButton></paper-fab>
 </form>
